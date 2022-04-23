a505c05d19b1a9511dc3d5bc375dc3f461e4140c
degalih <galihmramdan17@gmail.com>
2022-04-23 22:54:27 +0700

    fix error on encrypt database

--- core/lib/utils/encrypt.dart
+++ core/lib/utils/encrypt.dart
@@ -1,7 +1,7 @@
 import 'package:encrypt/encrypt.dart';
 
 String encrypt(String plainText) {
-  final key = Key.fromSecureRandom(32);
+  final key = Key.fromUtf8('eVwB9GKsJhA8v52c738RsPRuMs6SyJhw');
   final iv = IV.fromLength(16);
 
   final encrypter = Encrypter(AES(key));
